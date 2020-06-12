fc6c9f0441896cfd3751572344f9ff4b076c3820
asaebu <a.saebu@kw1c.nl>
2020-06-12 12:23:58 +0200

    crossorigin options set

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var io = require('socket.io')(http, {
+                                        origins: '*'
+                                    });
 
 var countPainters = 0;
 app.get('/', (req, res) => {
